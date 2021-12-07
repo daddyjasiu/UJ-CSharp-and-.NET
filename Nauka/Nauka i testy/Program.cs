30151af950590dc31bdc93641cb2595d40838345
Jan Skwarczek <jasiu.skwarczek@gmail.com>
2021-12-07 23:57:11 +0100

    ktor init

--- Nauka/Nauka i testy/Program.cs
+++ Nauka/Nauka i testy/Program.cs
@@ -1,45 +1,19 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Microsoft.VisualBasic;
 
 namespace Nauka_i_testy
 {
     public class Program
     {
-        public static bool IsAnagram(string s, string t)
-        {
 
-            if (s.Length != t.Length)
-                return false;
-            
-            Dictionary<char, int> dictionary = new();
-        
-            foreach(var c in s)
-            {
-                if(!dictionary.ContainsKey(c))
-                    dictionary.Add(c, 1);
-                else
-                    dictionary[c]++;
-            }
-            foreach(var c in t)
-            {
-                if(dictionary.ContainsKey(c))
-                    dictionary[c]--;
-            }
-            foreach(var pair in dictionary)
-            {
-                if(pair.Value != 0)
-                    return false;
-            }
-            return true;
-        }
-        
         public static void Main(string[] args)
         {
-            Console.WriteLine(IsAnagram("a", "ab"));
+            int[][] arr = {new []{1, 2}, new []{3, 4}, new []{5, 6}};
+
+            foreach (var i in arr)
+            {
+                Console.Write(i);
+            }
         }
     }
 }
